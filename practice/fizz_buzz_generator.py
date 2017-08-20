a985475aeb206259f1c4aa72c5aa588b78fba0e9
Edwin Carra <ecarra7979@gmail.com>
2017-08-19 22:39:36 -0400

    FizzBuzz game improved to generate game based on count_size.

---
+++ practice/fizz_buzz_generator.py
@@ -0,0 +1,15 @@
+
+from random import random
+
+# Change the count size to play the FizzBuzz game to what ever limit you choose
+# The count starts at 0 and end at 99
+count_size = 100
+for i in range(count_size):
+    if i % 5 == 0 and i % 3 == 0:
+        print("Fizz-Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
